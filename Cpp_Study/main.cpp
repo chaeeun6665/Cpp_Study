3e1d7471ad8503d779711208feb7fbdeb601790d
Chaeeun Lee <jenny6665@naver.com>
2025-01-16 00:54:28 +0900

    Initial Commit

---
+++ Cpp_Study/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Cpp_Study
+//
+//  Created by Jayce Lee on 1/16/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
