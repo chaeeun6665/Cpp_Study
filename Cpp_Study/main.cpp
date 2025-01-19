a049b25548afb456671111dafa466db183da4072
Chaeeun Lee <jenny6665@naver.com>
2025-01-19 14:00:59 +0900

    string 강의 기록

--- Cpp_Study/main.cpp
+++ Cpp_Study/main.cpp
@@ -6,19 +6,28 @@
 //
 
 #include <iostream>
+#include <locale>
 using namespace std;
 
 
+wchar_t wch = L'안';
+char str[] = {'h', 'e', 'l', 'l', 'o'};
+char str2[] = {'h', 'e', 'l', 'l', 'o', '\0'};
+
+char str3[] = "Hello World";
+wchar_t str3[] = L"Hello World"; //유니코드
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    
+//    bool isFemale;
+//    bool isMale;
+    
+//    cout << wch << endl;
+    wcout << wch << endl;
+    wcout.imbue(locale("ko_KR.UTF-8"));
+    cout << str << endl;
+    
     return 0;
     
-    if(isFemale != 1) {
-        
-    }
-    
-    if(isMale == false) { //가독성이 much better
-        
-    }
 }
---
+++ Cpp_Study/string-memo.hpp
@@ -0,0 +1,13 @@
+//
+//  string-memo.hpp
+//  Cpp_Study
+//
+//  Created by Jayce Lee on 1/19/25.
+//
+
+#ifndef string_memo_hpp
+#define string_memo_hpp
+
+#include <stdio.h>
+
+#endif /* string_memo_hpp */
